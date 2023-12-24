00948a3f2dcc4ff8134e1713d41991ac0bb09a17
OMAR K <mirxaumar1212@gmail.com>
2023-12-24 22:21:13 +0500

    4td commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,55 @@
-import { useState } from 'react';
-import './App.css'; 
+
+import React, { useState } from 'react';
+
+const products = [
+  {
+    id: 101,
+    title: "Krunch Burger",
+    desc: "Krunch fillet, spicy mayo, lettuce, sandwiched between a sesame seed bun",
+    image: "https://www.kfcpakistan.com/images/43a9fb50-ffaa-11ed-8180-812e571998fe-krunch_variant_0-2023-05-31115706.png",
+    price: 270,
+  },
+  {
+    id: 102,
+    title: "Zingeratha",
+    desc: "Tender boneless strips, sliced onions, tangy imli chutney, mint mayo, wrapped in a soft paratha",
+    image: "https://www.kfcpakistan.com/images/43a9fb50-ffaa-11ed-8180-812e571998fe-zingeratha_variant_0-2023-05-31115706.png",
+    price: 220,
+  },
+  {
+    id: 103,
+    title: "Rice & Spice",
+    desc: "Spiced and buttery rice with 6 pcs of Hot Shots topped with our signature Vietnamese sauce",
+    image: "https://www.kfcpakistan.com/images/43a95f10-ffaa-11ed-b673-4121381f04c6-rice-and-spice_variant_0-2023-05-31115706.png",
+    price: 320,
+  },
+];
 
 function App() {
-  const Product = [
-    {
-      id: 101,
-      title: "Krunch Burger",
-      desc: "Krunch fillet, spicy mayo, lettuce, sandwiched between a sesame seed bun",
-      image: "https://www.kfcpakistan.com/images/43a9fb50-ffaa-11ed-8180-812e571998fe-krunch_variant_0-2023-05-31115706.png",
-      price: 270,
-      qty: 5,
-    },
-    {
-      id: 102,
-      title: "Zingeratha",
-      desc: "Tender boneless strips, sliced onions, tangy imli chutney, mint mayo, wrapped in a soft paratha",
-      image: "https://www.kfcpakistan.com/images/43a9fb50-ffaa-11ed-8180-812e571998fe-zingeratha_variant_0-2023-05-31115706.png",
-      price: 220,
-      qty: 10,
-    },
-    {
-      id: 103,
-      title: "Rice & Spice",
-      desc: "Spiced and buttery rice with 6 pcs of Hot Shots topped with our signature Vietnamese sauce",
-      image: "https://www.kfcpakistan.com/images/43a95f10-ffaa-11ed-b673-4121381f04c6-rice-and-spice_variant_0-2023-05-31115706.png",
-      price: 320,
-      qty: 0,
-    }
-  ];
-  const [cart, setCart] = useState([]);
-  const [cartTotal, setCartTotal] = useState(0);
+  const [cart, setCart] = useState({ items: [], price: 0 });
   const [showCart, setShowCart] = useState(false);
 
   const addToCart = (selectedProduct) => {
-      setCart((prevCart) => {
-        const existingProduct = prevCart.find((item) => item.id === selectedProduct.id);
+    setCart((prevCart) => {
+      const existingProduct = prevCart.items.find((item) => item.id === selectedProduct.id);
 
-        if (existingProduct) {
-          const updatedCart = prevCart.map((item) =>
-            item.id === selectedProduct.id ? { ...item, qty: item.qty + 1 } : item
-          );
-          return updatedCart;
-        } else {
-          const productToAdd = { ...selectedProduct, qty: 1 };
-          return [...prevCart, productToAdd];
-        }
-      });
-
-      setCartTotal((prevTotal) => prevTotal + selectedProduct.price);
+      if (existingProduct) {
+        const updatedCart = prevCart.items.map((item) =>
+          item.id === selectedProduct.id ? { ...item, qty: item.qty + 1 } : item
+        );
+        return { items: updatedCart, price: prevCart.price + selectedProduct.price };
+      } else {
+        const productToAdd = { ...selectedProduct, qty: 1 };
+        return { items: [...prevCart.items, productToAdd], price: prevCart.price + selectedProduct.price };
+      }
+    });
   };
 
   const toggleCart = () => {
     setShowCart(!showCart);
   };
 
-  return  (
+  return (
     <div className="container my-5">
       <nav className="navbar navbar-light bg-light">
         <div className="container-fluid">
@@ -62,7 +57,7 @@ function App() {
             className="navbar-brand btn btn-light text-dark"
             onClick={toggleCart}
           >
-            <h3>🛒 Cart {cart.length} | 💵 Rs.{cartTotal}</h3>
+            <h3>🛒 Cart {cart.items.length} | 💵 Rs.{cart.price}</h3>
           </button>
         </div>
       </nav>
@@ -78,7 +73,7 @@ function App() {
               </tr>
             </thead>
             <tbody>
-              {cart.map((cartItem) => (
+              {cart.items.map((cartItem) => (
                 <tr key={cartItem.id}>
                   <td>{cartItem.title}</td>
                   <td>{cartItem.qty}</td>
@@ -91,7 +86,7 @@ function App() {
       )}
 
       <div className="row mt-4">
-        {Product.map((product) => (
+        {products.map((product) => (
           <div key={product.id} className="col-md-4 mb-4">
             <div className="card">
               <img src={product.image} className="card-img-top" alt={product.title} />
